6b876863e55cf11bf11061f8f2e56b62fadf968c
Bogdan-Chernov <72433961+Bogdan-Chernov@users.noreply.github.com>
2020-10-13 14:05:35 +0300

    Initial commit

---
+++ Пр3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MetClass;
+
+namespace Пр3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassSTO func = new ClassSTO();
+            func.ggg();
+            Console.WriteLine("1 - Сложение");
+            Console.WriteLine("2 - Вычитание");
+            int g = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine(" ");
+            
+            if (g <= 1)
+
+            {
+                Console.WriteLine($"Результат сложения: {func.addition()}");
+                Console.ReadLine();
+            }
+
+            else if (g >= 2)
+            {
+                Console.WriteLine($"Результат вычитание: {func.Subtraction()}");
+                Console.ReadLine();
+            }
+          
+            }
+        }
+    }
+
